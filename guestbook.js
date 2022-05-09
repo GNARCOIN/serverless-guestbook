5ca55363f6fe88af6916a8669642f4652eff7e12
GNARCOIN <89608197+GNARCOIN@users.noreply.github.com>
2022-05-09 16:15:35 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://139a82f1.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
